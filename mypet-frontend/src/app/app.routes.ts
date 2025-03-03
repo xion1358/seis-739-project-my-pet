6fd9394905b2b0e3122dd37064a84896f94b262e
xion1358 <xion1358@stthomas.edu>
2025-03-03 16:52:01 -0600

    Added registration page

--- mypet-frontend/src/app/app.routes.ts
+++ mypet-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 export const appRoutes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'log-in', component: LogInComponent},
+  { path: 'registration', component: RegistrationComponent},
 ];
--- mypet-frontend/src/app/landing-page/landing-page.component.ts
+++ mypet-frontend/src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
